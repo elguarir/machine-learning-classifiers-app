78df2bd71ced4a1e45352e21b60ac12afd39354f
MohamedElguarir <120427922+MohamedElguarir@users.noreply.github.com>
2024-03-21 04:23:14 +0000

    feat: added initial functionality (ability to train the model + classify)

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,23 @@
 from flask import Flask, request, jsonify
-from flower_classifier_trees import FlowerClassifierTrees
+from flower_classifier import FlowerClassifierTrees
+from flower_classifier import FlowerClassifierRandomForest
+from flask_cors import CORS
 
 clfTrees = FlowerClassifierTrees()
+clfForest = FlowerClassifierRandomForest()
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/trees/train", methods=["GET"])
 def train_flower_classifier():
     accuracy = clfTrees.train()
+    return jsonify({"accuracy": accuracy})
+
+
+@app.route("/random_forest/train", methods=["GET"])
+def train_flower_classifier_forest():
+    accuracy = clfForest.train()
     return jsonify({"accuracy": accuracy})
 
 
@@ -27,10 +37,37 @@ def classify_flower():
     ):
         return jsonify({"error": "Missing features"}), 400
 
-    if not hasattr(clfTrees, "clf"):
+    if not hasattr(clfTrees, "classifier"):
         return jsonify({"error": "Model has not been trained yet"}), 400
 
     prediction = clfTrees.predict(
+        [sepal_length, sepal_width, petal_length, petal_width]
+    )
+    print("prediction", prediction)
+
+    return jsonify({"class": prediction})
+
+
+@app.route("/random_forest/classify", methods=["POST"])
+def classify_flower_forest():
+    data = request.get_json()
+    sepal_length = data.get("sepal_length")
+    sepal_width = data.get("sepal_width")
+    petal_length = data.get("petal_length")
+    petal_width = data.get("petal_width")
+
+    if (
+        sepal_length is None
+        or sepal_width is None
+        or petal_length is None
+        or petal_width is None
+    ):
+        return jsonify({"error": "Missing features"}), 400
+
+    if not hasattr(clfForest, "classifier"):
+        return jsonify({"error": "Model has not been trained yet"}), 400
+
+    prediction = clfForest.predict(
         [sepal_length, sepal_width, petal_length, petal_width]
     )
     print("prediction", prediction)
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import axios from "axios";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const api = axios.create({
+  baseURL: "http://127.0.0.1:5000",
+});
